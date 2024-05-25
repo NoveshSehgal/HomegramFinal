9da5a9620a511ab4ba23885aaaf5f077e87d613d
NoveshSehgal <noveshsehgal18@gmail.com>
2024-05-25 15:05:16 +0530

    mobile compatible

--- script.js
+++ script.js
@@ -98,3 +98,11 @@ indicators.forEach((item, i) => {
         currentTestimonial = i;
     })
 })
+
+function toggleMenu() {
+  const menuLinks = document.querySelector('.menu-links');
+  const hamburgerIcon = document.querySelector('.hamburger-icon');
+
+  menuLinks.classList.toggle('open');
+  hamburgerIcon.classList.toggle('open');
+}
